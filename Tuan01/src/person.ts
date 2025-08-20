5ead1fbaadf068146e51557612d6e0ddeb532b81
dgkhoa279 <vodangkhoa270904@gmail.com>
2025-08-20 21:07:46 +0700

    cau2

--- Tuan01/src/person.ts
+++ Tuan01/src/person.ts
@@ -6,3 +6,16 @@ class Person {
 }
 const p1:Person = new Person("Khoa",21)
 p1.displayInfo()
+
+class Student extends Person {
+    constructor(name: string,age: number,public grade:string){
+        super(name,age)
+    }
+    displayInfo(): void {
+        console.log(`Name: ${this.name},Age: ${this.age},Grade: ${this.grade}`)
+    }
+}
+
+
+const student1:Student = new Student("Khoa",21,"DHKTPM18A")
+console.log(student1)
