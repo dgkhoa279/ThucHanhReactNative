fe0367ca810d4233e100a9d4430d3f12fdee681f
dgkhoa279 <vodangkhoa270904@gmail.com>
2025-08-21 08:09:08 +0700

    cau18

---
+++ Tuan01/src/mathUtil.ts
@@ -0,0 +1,16 @@
+class MathUtil {
+    static add(a: number, b: number): number {
+        return a + b
+    }
+    static sub(a: number, b: number): number {
+        return a - b
+    }
+    static mul(a: number, b: number): number {
+        return a * b
+    }
+    static div(a: number, b: number): number {
+        return a / b
+    }
+}
+console.log(MathUtil.add(2,3));
+console.log(MathUtil.div(2,0));
