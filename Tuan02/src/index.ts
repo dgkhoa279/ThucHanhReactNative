7aceba463f732f9ba520756b3ac3c0ad718b8f84
H82M19\Student <Student>
2025-08-27 07:42:55 +0700

    Bai 2

--- Tuan02/src/index.ts
+++ Tuan02/src/index.ts
@@ -1,7 +1,17 @@
 // Bài 1 
-const helloAsync = new Promise((resolve)=> {
-    setTimeout(()=> {
-        resolve("Hello,I'm Vo Dang Khoa")
-    },2000)
-})
-helloAsync.then(console.log)
+// const helloAsync = new Promise((resolve)=> {
+//     setTimeout(()=> {
+//         resolve("Hello,I'm Vo Dang Khoa")
+//     },2000)
+// })
+// helloAsync.then(console.log)
+
+// Bài 2 
+function getNumber(){
+    return new Promise((resolve)=> {
+        setTimeout(()=> {
+            resolve(10)
+        },1000)
+    })
+}
+getNumber().then(console.log)
